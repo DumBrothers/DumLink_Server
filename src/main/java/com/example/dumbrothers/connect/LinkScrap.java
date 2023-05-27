7c298a4151d171d0c9496f9862bd0a9a0d38915f
isoProject <jj99526@naver.com>
2023-05-27 17:00:14 +0900

    태그추가한것

---
+++ src/main/java/com/example/dumbrothers/connect/LinkScrap.java
@@ -0,0 +1,56 @@
+package com.example.dumbrothers.connect;
+
+import org.jsoup.Jsoup;
+import org.jsoup.select.Elements;
+import org.springframework.data.util.Pair;
+
+import org.jsoup.nodes.Document;
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+
+
+public class LinkScrap {
+    public static Map<String, String> handleSendText(String url) throws IOException {
+        Map<String, String> ogMap = new LinkedHashMap<>();
+        Document document = Jsoup.connect(url).userAgent("Android").get();
+        Elements elements = document.select("meta[property^=og:]");
+        if (elements != null) {
+            for (int i = 0; i < elements.size(); i++) {
+                switch (elements.get(i).attr("property")) {
+                    case "og:image":
+                        String content = elements.get(i).attr("content");
+                        if (content != null) {
+                            ogMap.put("image", content);
+                            break;
+                        }
+                    case "og:title":
+                        if (elements.get(i).attr("content") != null) {
+                            ogMap.put("title", elements.get(i).attr("content"));
+                            break;
+                        }
+                    case "og:url":
+                        if (elements.get(i).attr("content") != null) {
+                            ogMap.put("url", elements.get(i).attr("content"));
+                            break;
+                        }
+                    case "og:description":
+                        if (elements.get(i).attr("content") != null) {
+                            ogMap.put("description", elements.get(i).attr("content"));
+                            break;
+                        }
+                    case "og:site_name":
+                        if (elements.get(i).attr("content") != null) {
+                            ogMap.put("site_name", elements.get(i).attr("content"));
+                            break;
+                        }
+                }
+            }
+        }
+
+        return ogMap;
+    }
+}
+
+
--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into article(id,title,content) values (1,'ㄱㄱㄱㄱ','111111');
-insert into article(id,title,content) values (2,'sssss','211111');
-insert into article(id,title,content) values (3,'ㄷㄷㄷㄷㄷ','311111');
