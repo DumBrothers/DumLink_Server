9873a0388e60d4f5005f408e69bc5ab478f7a6d2
Pushgyu <ghfn123@gmail.com>
2023-06-08 20:32:28 +0900

    modify title parsing error

--- src/main/java/com/example/dumbrothers/connect/LinkScrap.java
+++ src/main/java/com/example/dumbrothers/connect/LinkScrap.java
@@ -13,7 +13,7 @@ import java.util.Map;
 
 
 public class LinkScrap {
-    private final static String DEFAULT_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36 Android";
+    private final static String DEFAULT_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36";
     public static Map<String, String> handleSendText(String url) throws IOException {
 
         Map<String, String> ogMap = new LinkedHashMap<>();
